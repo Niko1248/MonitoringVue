d215dd9a97ecd7bc64b601ebd04bbb0940b6a073
Назар Усацкий <usackiy95@gmail.com>
2024-01-08 16:59:43 +0300

    Деле

--- Frontend/monitoring/src/components/NavMenu.vue
+++ Frontend/monitoring/src/components/NavMenu.vue
@@ -9,21 +9,28 @@
 			<label for="workSorted">Исправные</label>
 			<input type="checkbox" id='alarmSorted' v-model="alarmSorted">
 			<label for="alarmSorted">Неисправные</label>
-			<img class="search" src="../assets/SEARCH.svg" @click="showSearch" alt="Поиск">
-			<input 
-				v-if="isSearch" 
-				type="text" 
-				v-model="searchQuery"
-				@input = updateInput
-			>
-			<div class="log_text">Log</div>
-			<img class="settings" src="../assets/SETTINGS.svg" alt="">
-			<img class="printPage" src="../assets/PRINT.svg" alt="">
-			<div class='test' @click="$store.commit('disableSound')">Сброс</div>
+			<div class="search">
+				<transition name="fade">
+					<input 
+							v-if="isSearch" 
+							type="text" 
+							class="search_input"
+							v-model="searchQuery"
+							@input = updateInput
+					>
+				</transition>
+				<img class="search_ico ico" src="../assets/SEARCH.svg" @click="showSearch" alt="Поиск">
+			</div>
+			<div class="log_text ico">Log</div>
+			<img class="settings ico" src="../assets/SETTINGS.svg" alt="">
+			<img class="printPage ico" src="../assets/PRINT.svg" alt="">
+			<div class='reset' @click="$store.commit('disableSound')">
+				<img class="reset_ico ico" src="../assets/RESET.svg" alt="">
+			</div>
 		</div>
 		<div class="auth">
 			<a class="auth_text" href="">user</a>
-			<img src="../assets/USER.svg" alt="">
+			<img class="auth_ico ico" src="../assets/USER.svg" alt="">
 		</div>
 	</div>
 </template>
@@ -47,23 +54,23 @@ export default {
 		}
 	},
 	computed: {
-    workSorted: {
-      get() {
-        return this.$store.state.workSorted;
-      },
-      set(value) {
-        this.$store.commit('disableWorkSorted', value);
-      }
-    },
-    alarmSorted: {
-      get() {
-        return this.$store.state.alarmSorted;
-      },
-      set(value) {
-        this.$store.commit('disableAlarmSorted', value);
-      }
-    }
-  }
+		workSorted: {
+			get() {
+				return this.$store.state.workSorted;
+			},
+			set(value) {
+				this.$store.commit('disableWorkSorted', value);
+			}
+		},
+		alarmSorted: {
+			get() {
+				return this.$store.state.alarmSorted;
+			},
+			set(value) {
+				this.$store.commit('disableAlarmSorted', value);
+			}
+		}
+	}
 }
 </script>
 <style scoped lang="scss">
@@ -76,6 +83,16 @@ export default {
 	font-family: Wix Madefor Display;
 
 
+}
+
+
+.search_ico,
+.settings,
+.printPage,
+.reset_ico {
+	margin-right: 25px;
+	width: 20px;
+	cursor: pointer;
 }
 
 .logo {
@@ -93,31 +110,70 @@ export default {
 	font-size: 20px;
 	text-decoration: none;
 	color: white;
+}
 
+.search {
+	position: relative;
+}
+
+.search_input {
+	position: absolute;
+	width: 182px;
+	left: -200px;
+	height: 26px;
+	border-radius: 10px 10px 0px 10px;
+	border: none !important;
+	padding-left: 10px;
+	font-size: 16px;
+	color: #0E1621;
+
+	&:focus {
+		outline: none !important;
+	}
+}
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+
+.search_ico {
+	padding-top: 2px;
+	width: 18px;
 }
 
 .menu {
 	display: flex;
 	align-items: center;
 	justify-content: space-around;
-	width: 30%;
+	width: auto;
 }
 
 .log_text {
 	color: white;
-	font-size: 24px;
+	font-size: 20px;
+	margin-right: 30px;
+	cursor: pointer;
+	margin-bottom: 5px;
 }
 
-.search,
-.log_text,
-.settings,
-.printPage {
-	width: 20px;
-}
+
+
+
 
 .auth {
 	display: flex;
 	align-items: center;
+}
+
+.auth_ico {
+	width: 30px;
+
 }
 
 .auth_text {
@@ -127,8 +183,9 @@ export default {
 	text-transform: uppercase;
 }
 
-.test {
-	color: white;
-	cursor: pointer;
+@media (max-width:768px) {
+	.menu {
+		margin-left: 35%;
+	}
 }
 </style>
