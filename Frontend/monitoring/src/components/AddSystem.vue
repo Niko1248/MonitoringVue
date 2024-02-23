c24b6d4f74bb63e32a67bc56e7f27056e99a8f47
Niko1248 <Sirius04@bk.ru>
2024-02-23 20:20:55 +0300

    Переделал добавление СП

--- Frontend/monitoring/src/components/AddSystem.vue
+++ Frontend/monitoring/src/components/AddSystem.vue
@@ -4,46 +4,41 @@
 					<div class="close" @click="showPopupSP">
 							<img src="./../assets/img/nav/close.svg" alt="закрыть" width="20px">
 					</div>
-					<div class="Sp__wrapper-header">
-							
-							<div type="text" class="correspondent__wrap">
-								<input placeholder="Введите номер СП" style="margin-right: 1vw;">
-								<h1 class="systemCorrespondent"> Колибри - {{ systemData.systemCorrespondent }}</h1>
-								<input type="text" placeholder="Введите корреспондента" style="margin-right: 1vw;">
+					<div class="flex__wrap">
+						<div class="form-left">
+							<h1>Добавление системы передачи</h1>
+							<div class="main__form">
+								<input type="text" placeholder="№ СП">
+								<input type="text" placeholder="Корреспондент">
+								<input type="text" placeholder="№ pin">
+								<input type="text" placeholder="КМУ/ОМУ">
 							</div>
+							<h2>Загрузка системы передачи</h2>
+							<div class="payload__form">
+								<input type="text" placeholder="Номер канала">
+								<input type="text" placeholder="Корреспондент">
+								<select name="" id="">
+									<option value="" disabled selected hidden>Вид связи</option>
+									<option value="Вершина">Вершина</option>
+									<option value="Монолит">Монолит</option>
+								</select>
+								<img src="../assets/plus.svg" width="25px" height="25px" style="margin-left: 1vw;" alt="">
+							</div>
+							<div class="payload__list">
+								<p>Добавить нагрузку</p>
+							</div>
+							<h2>Примечания</h2>
+							<div class="note"></div>
+						</div>
+						<div class="form__right">
+							<img src="../assets/img/settings/Kolibri.png" width="75%" alt="Лого">
+							<h2>Линейный тракт</h2>
+							<select name="" id="">
+								<option value="">Основной линейный тракт</option>
+							</select>
+						</div>
 					</div>
-					<div class="Sp__wrapper-content">
-							<div class="payload__wrap">
-								<div class="payload__content"></div>
-								<form action="" class="payload__form">
-									<input type="text" placeholder="Введите номер КС" class="payload__input">
-									<input type="text" placeholder="Корреспондент" class="payload__input">
-									<select name="" id="">
-										<option value="" disabled selected hidden>Выберите тип</option>
-										<option value="">Вершина</option>
-										<option value="">Монолит</option>
-									</select>
-									<button class="payload__btn">Добавить</button>
-								</form>
-							</div>
-							<div class="note__wrap">
-								<div class="note__form">
-									<input class="payload__input" type="text" placeholder="Введите номер пина">
-									<select name="" id="">
-										<option value="" disabled selected hidden>КМУ/ОМУ</option>
-										<option value="КМУ">КМУ</option>
-										<option value="ОМУ">ОМУ</option>
-									</select>
-									<input class="payload__input" type="text" placeholder="Введите КМУ/ОМУ">
-									
-								</div>
 
-								<div class="Sp__note">
-									<textarea cols="30" rows="10" placeholder="Примечания"></textarea>
-								</div>
-							</div>
-							
-					</div>
 					<button class="save">Сохранить</button>
 			</div>
 	</div>
@@ -92,21 +87,7 @@ export default {
 </script>
 	
 <style lang="scss" scoped>
-input {
-	background-color: #2E3541;
-	border-radius: 5px;
-	border: 0;
-	color: white;
-	font-size: 16px;
-	padding-left: 1vw;
-}
-input::placeholder{
-	color: white;
-	text-align: left;
-}
-.payload__input{
-	padding: 1vw;
-}
+
 .SystemP {
 	font-family: Wix Madefor Display;
 	width: 100vw;
@@ -128,38 +109,71 @@ input::placeholder{
 	border-radius: 10px;
 	filter: drop-shadow(0px 20px 30px rgba(0, 0, 0, 0.4274509804));
 }
-.ico{
-width: 20px;
-height: 20px;
-margin-right: 5px;
+.flex__wrap {
+	display: flex;
+	gap: 2vw;
+	max-width: 96%;
+	height: 96%;
+	justify-content: space-between;
 }
-.Sp__content-items{
-margin-bottom: 5px;
+.form-left{
+	max-width: 63%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+}
+.form__right{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+h1, h2{
+	color: white;
+	margin-bottom: 1vw;
+}
+.main__form{
+	display: grid;
+  grid-template-columns: 20% 40% 20% 20%;
+	margin: 0 0 1vw 0;
 }
 .payload__form{
-	display: flex;
-	flex-direction: column;
-	height: 35%;
+	display: grid;
+	grid-template-columns: 30% 30% 30% 10%;
+	align-items: center;
 	justify-content: space-between;
+	margin: 0 0 1vw 0;
+
 }
-.payload__text{
-color: #DBFFDA;
-}
-.correspondent__wrap{
-	display: flex;
-}
-.payload__wrap{
-	width: 30%;
-	display: flex;
-	flex-direction: column;
-}
-.note__form{
-	display: flex;
-	flex-direction: column;
-	height: 26%;
+.payload__list, .note{
+	background-color: #2E3541;
+	border-radius: 5px;
+	border: 0;
+	height: 30%;
+	color: rgb(217, 217, 217, 17%);
+	padding: 0.7vw;
+	font-size: 18px;
 	margin-bottom: 1vw;
-	justify-content: space-between;
 }
+.note{
+	height: 15%;
+	color: white;
+}
+input {
+	background-color: #2E3541;
+	border-radius: 5px;
+	border: 0;
+	color: white;
+	font-size: 16px;
+	padding: 0.7vw;
+	margin-right: 1vw;
+}
+input::placeholder{
+	color: rgb(217, 217, 217, 17%);
+	text-align: left;
+	font-size: 18px;
+	font-weight: 400;
+}
+
 .close {
 	height: 100%;
 	width: 3%;
@@ -203,58 +217,6 @@ color: #DBFFDA;
 	}
 }
 
-.Sp__wrapper-header {
-	display: flex;
-	align-items: center;
-	justify-content: start;
-	color: #fff;
-	font-family: Wix Madefor Display;
-	margin-bottom: 2vw;
-}
-
-.Sp__wrapper-content {
-	display: flex;
-	gap: 1vw;
-	height: 85%;
-}
-.item__wrapper{
-display: flex;
-}
-.systemNumber {
-	font-family: Viga Regular;
-	margin-right: 2vw;
-	font-size: 3vw;
-	font-weight: 100;
-}
-
-.systemCorrespondent {
-	margin-right: 1vw;
-
-}
-
-.systemState {
-	display: block;
-	padding: 0.1vw 0.7vw 0.3vw 0.7vw;
-	border-radius: 7px;
-	margin-top: 5px;
-}
-.payload__btn {
-	background: #053429;
-	outline: none;
-	color: #F2F1F1;
-	width: 100%;
-	height: 35px;
-	border: none;
-	border-radius: 10px;
-	font-size: 18px;
-	transition: 0.2s ease-in;
-
-	&:hover {
-			filter: drop-shadow(0px -2px 5px #ffffff34);
-			transition: 0.2s ease-in;
-			color: #fff;
-	}
-}
 .save {
 	background: #053429;
 	outline: none;
@@ -333,19 +295,16 @@ textarea {
 }
 
 select {
-	width: calc(86% + 40px);
-	height: 45px;
 	border: none;
 	border-radius: 5px;
 	outline: none;
 	background: #2E3541;
 	color: #F2F1F1;
-	padding: 0 10px;
+	padding: 0.65vw;
 	font-size: 18px;
-	color: #F2F1F1;
 	transition: 0.2s ease-in;
-	margin-right: 10px;
-
+	margin-right: 1vw;
+	width: 100%;
 	&:hover {
 			filter: drop-shadow(0px 0px 2px #ffffff40);
 			transition: 0.2s;
@@ -359,103 +318,5 @@ select {
 	}
 }
 
-.payload__content {
-	height: 60%;
-	background: #2E3541;
-	border-radius: 5px;
-	margin-bottom: 2vw;
-}
-
-.Sp__tract {
-	width: 30%;
-	height: 71vh;
-}
-
-.note__wrap {
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	width: 30%;
-}
-
-.remove_wrapper {
-	display: flex;
-
-	div {
-			margin-top: 5px;
-			display: flex;
-			margin-left: 1vw;
-			background: #b70000;
-			border-radius: 7px;
-			padding: 0 0 0 0.7vw;
-	}
-
-	p:nth-child(2) {
-			margin-bottom: 0.1vw;
-			display: block;
-			padding: 0.1vw 0vw 0.1vw 0vw;
-			height: 90%;
-			width: 3vw;
-			margin-left: 1vw;
-			border-radius: 7px 0px 0px 7px;
-			text-align: center;
-			transition: 0.1s;
-
-			&:hover {
-					background: #0000009b;
-					transition: 0.1s;
-
-			}
-	}
-
-	p:nth-child(3) {
-			display: block;
-			padding: 0.1vw 0vw;
-			height: 90%;
-			width: 3vw;
-			border-radius: 0px 7px 7px 0px;
-			text-align: center;
-			margin-bottom: 0.1vw;
-			transition: 0.1s;
-
-			&:hover {
-					transition: 0.1s;
-					background: #0000009b;
-
-			}
-
-	}
-}
-
-.remove__ico {
-	width: 1.5vw;
-	margin-left: 2vw;
-	transition: 0.2s;
-
-	&:hover {
-			filter: drop-shadow(0 0 10px #fff);
-			stroke: #fff;
-			transition: 0.2s;
-	}
-
-	&:active {
-			filter: drop-shadow(0 0 10px #000);
-			stroke: #fff;
-			transition: 0.2s;
-	}
-}
-
-.fade-enter-active,
-.fade-leave-active {
-	transition: 1.5s ease;
-	opacity: 1;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-	opacity: 0;
-	transition: 1.5s ease;
-
-}
 </style>
 	
